b95eb32e0f6f647f7209ebfc703d72d933d1ecb4
Richard Elberger <elberger@amazon.com>
2019-08-16 07:56:26 -0700

    init

---
+++ lambda-issuer-iotcore/customize.py
@@ -0,0 +1,14 @@
+import boto3
+
+# The Cloudformation creates the policy. 
+def applyPolicy(principal, policy_name):
+    return
+
+def applyThing(principal, thing_name):
+    return
+
+def applyThingGroup(group_name, thing_name):
+    return
+
+def createThing(thing_name):
+    return
