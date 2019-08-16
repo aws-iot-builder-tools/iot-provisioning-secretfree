c9e9318b49d716fbfeeb03e2a192e5dbd77fd107
Richard Elberger <elberger@amazon.com>
2019-08-16 07:53:44 -0700

    remove unnecessary code since storing pubkey on s3 was removed, clean up parameters

--- lambda-issuer-acmpca/main.py
+++ lambda-issuer-acmpca/main.py
@@ -6,30 +6,11 @@ import os
 import OpenSSL.crypto
 from OpenSSL.crypto import load_certificate_request, FILETYPE_PEM, dump_publickey
 
-def get_pubkey( req ):
-    device_id = req.get_subject().CN
-
-    d = boto3.client('dynamodb')
-    s3 = boto3.resource('s3')
-    
-    response = d.get_item(
-        Key={ 'device-id': { 'S' : device_id } },
-        TableName='widgiot-public-keys'
-    )
-
-    s3_bucket = response['Item']['pubkey-bucket']['S']
-    s3_object = response['Item']['pubkey-object']['S']
-    content_object = s3.Object(s3_bucket, s3_object)
-    file_content = content_object.get()['Body'].read().decode('utf-8')
-
-    return file_content
-
-def provision_certificate( csr, pubkey ):
+def provision_certificate( csr ):
     acmpca = boto3.client('acm-pca')
     ca_arn = os.environ['ACMPCA_CA_ARN']
         
-    # Create the Certificate
-    # TODO: Automatically compute days remaining ... somehow?
+    # Create the Certificate - duration 150 days - very arbitrary
     cert = acmpca.issue_certificate(
         CertificateAuthorityArn=ca_arn,
         SigningAlgorithm='SHA256WITHRSA',
@@ -60,8 +41,6 @@ def lambda_handler(event, context):
     print(context)
     csr = base64.b64decode(event['headers']['device-csr'])
     req = load_certificate_request( FILETYPE_PEM, csr )
-
-    pubkey = get_pubkey(req)
-    response = provision_certificate(csr, pubkey)
+    response = provision_certificate( csr )
 
     return response['Certificate']
